0ddd0d4469a7cf658af1db5c14cb47085629e0ab
Marcelo Calegare <marcelocalegare06@gmail.com>
2024-08-29 01:32:11 -0300

    Update impar-ou-par.py

--- impar-ou-par.py
+++ impar-ou-par.py
@@ -1,24 +1,29 @@
 from random import randint
 
+print('=-'*20)
 print("""DIGITE UMA OPÇÃO:          
-[1] PAR
-[2] IMPAR""")
+[1] IMPAR
+[2] PAR""")
 
-opc = int(input('ESCOLHA UMA: '))
+opc = int(input('\nQUAL SUA ESCOLHA? '))
 
 n = int(input('Digite um numero de 0 a 10: '))
 
 computador = randint(0, 10)
-
-print(f'O Jogador escolheu {n} e a Maquina escolheu {computador} a soma é {n+computador} ')
-
+print('=-'*20)
+print(f'O Jogador escolheu {n} e a Maquina escolheu {computador}!')
+if (n + computador) % 2 == 0:
+    print(f'A soma é {n+computador}, esse numero é PAR!')
+else:
+    print(f'A soma é {n+computador}, esse numero é IMPAR!')
 if opc == 1:
-    if (n + computador) % 2 == 0:
+    if (n + computador) % 2 == 1:
         print('O jogador GANHOU!')
     else:
         print('A maquina GANHOU!')
 if opc == 2:
     if (n + computador) % 2 == 0:
-        print('A maquina GANHOU!')
-    else:
         print('O jogador GANHOU')
+    else:
+        print('A maquina GANHOU!')
+print('=-'*20)
